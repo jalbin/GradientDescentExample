a629db5fc2b4239f209f119c52c097dff13acd92
Jules Albin <julesalbin@orange.fr>
2024-01-03 03:42:00 +0100

    Amending code from  python version 2 to version 3

--- gradient_descent_example.py
+++ gradient_descent_example.py
@@ -1,4 +1,4 @@
-from numpy import *
+import numpy as np
 
 # y = mx + b
 # m is slope, b is y-intercept
@@ -27,19 +27,20 @@ def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_i
     b = starting_b
     m = starting_m
     for i in range(num_iterations):
-        b, m = step_gradient(b, m, array(points), learning_rate)
+        b, m = step_gradient(b, m, np.array(points), learning_rate)
     return [b, m]
 
 def run():
-    points = genfromtxt("data.csv", delimiter=",")
+    points = np.genfromtxt("data.csv", delimiter=",")
     learning_rate = 0.0001
-    initial_b = 0 # initial y-intercept guess
-    initial_m = 0 # initial slope guess
+    initial_b = 0  # initial y-intercept guess
+    initial_m = 0  # initial slope guess
     num_iterations = 1000
-    print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
-    print "Running..."
+    print("Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
+    print("Running...")
     [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
+    print("After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points)))
 
 if __name__ == '__main__':
     run()
+
